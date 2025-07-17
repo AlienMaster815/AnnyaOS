8757659dcd59a2f285bace164f0c74afb9c07000
AlienMaster815 <126557457+AlienMaster815@users.noreply.github.com>
2025-07-16 23:23:19 -0400

    Update MouseDevice.c

--- kernel/DeviceManaging/VrtualDevices/MouseDevice.c
+++ kernel/DeviceManaging/VrtualDevices/MouseDevice.c
@@ -3,24 +3,17 @@
 static PSYSTEM_STATE_STACK SystemState = 0x00;
 //static bool FirstDraw = true;
 
-void LouKeMouseMoveEventUpdate(int64_t X, int64_t Y, BOOL RightClick, BOOL LeftClick){
-    if(SystemState){
-        SystemState->MouseState.MouseX += X;
-        SystemState->MouseState.MouseY += Y;
-    }
-    PLOUSINE_USER_SHARED_MESSAGE Message = LouKeCreateNewUserMessage(
+PLOUSINE_USER_SHARED_MESSAGE LouKeMouseAllocateMessageDevice(){
+    return LouKeCreateNewUserMessage(
         MOUSE_EVENT_TYPE,
         HIGH_PRIORITY
     );
- 
-    Message->MouseEvent.X = X;
-    Message->MouseEvent.Y = Y;
-    Message->MouseEvent.RightClick = RightClick;
-    Message->MouseEvent.LeftClick = LeftClick;
+}
+
+void LouKeMouseMoveEventUpdate(PLOUSINE_USER_SHARED_MESSAGE Message){
     LouKeDispatchNewUserMessage(
         Message
     );
-
 }
 
 
