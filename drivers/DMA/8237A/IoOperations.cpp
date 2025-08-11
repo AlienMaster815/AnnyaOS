22261672b6592b5ee6cde4e6ffc75fbfa3cac963
AlienMaster815 <tgrenier.815@gmail.com>
2025-08-11 19:55:59 -0400

    DMA16 update

--- drivers/DMA/8237A/IoOperations.cpp
+++ drivers/DMA/8237A/IoOperations.cpp
@@ -2,50 +2,41 @@
 
 static spinlock_t IoLock = {0};
 
-//just to make my life easier
-static inline LOUSTATUS ReleaseIoLock(LOUSTATUS Status, LouKIRQL* Irql){
+static inline LOUSTATUS ReleaseIoLock(LOUSTATUS Status, LouKIRQL* Irql) {
     LouKeReleaseSpinLock(&IoLock, Irql);
     return Status;
 }
 
+static const UINT16 DmaBaseAddrReg[8] = {
+    0, DMA16_ST_ADDR_REG_C1, DMA16_ST_ADDR_REG_C2, DMA16_ST_ADDR_REG_C3,
+    0, DMA16_ST_ADDR_REG_C5, DMA16_ST_ADDR_REG_C6, DMA16_ST_ADDR_REG_C7
+};
 
-LOUSTATUS Write8237AStartAddressRegister(UINT8 Channel, UINT16 Address){
+static const UINT16 DmaPageReg[8] = {
+    0, C1_PAGE_ADDRESS_REGISTER, C2_PAGE_ADDRESS_REGISTER, C3_PAGE_ADDRESS_REGISTER,
+    0, C5_PAGE_ADDRESS_REGISTER, C6_PAGE_ADDRESS_REGISTER, C7_PAGE_ADDRESS_REGISTER
+};
+
+LOUSTATUS Write8237AStartAddress(UINT8 Channel, UINT32 DmaAddress) {
+    if (Channel > 7 || Channel == 0 || Channel == 4) {
+        LouPrint("Invalid Channel %u\n", Channel);
+        return STATUS_INVALID_PARAMETER;
+    }
+
     LouKIRQL Irql;
     LouKeAcquireSpinLock(&IoLock, &Irql);
-    switch(Channel){
-        case 1:
-            outw(DMA16_ST_ADDR_REG_C1, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        case 5:
-            outw(DMA16_ST_ADDR_REG_C5, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        case 2:
-            outw(DMA16_ST_ADDR_REG_C2, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        case 6:
-            outw(DMA16_ST_ADDR_REG_C6, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        case 3:
-            outw(DMA16_ST_ADDR_REG_C3, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        case 7:
-            outw(DMA16_ST_ADDR_REG_C7, Address);
-            return ReleaseIoLock(STATUS_SUCCESS, &Irql);
-        default:
-            LouPrint("Invalid Channel\n");
-            break;
-    }
-    return ReleaseIoLock(STATUS_INVALID_PARAMETER, &Irql);
-}
 
-
-
-LOUSTATUS Write8237AStartAddress(UINT8 Channel, UINT32 DmaAddress){
-    LOUSTATUS Status = Write8237AStartAddressRegister(Channel, DmaAddress & UINT16_MAX);
-    if(Status != STATUS_SUCCESS){
-        return Status;
+    UINT32 AddrForController = DmaAddress;
+    if (Channel >= 5) {
+        AddrForController >>= 1;
     }
 
+    UINT8 Controller = (Channel < 4) ? 0 : 1;
+    outb(DMA_CLEAR_FF_REG(Controller), 0x00);
 
-    return STATUS_SUCCESS;
+    outb(DmaBaseAddrReg[Channel], AddrForController & 0xFF);
+    outb(DmaBaseAddrReg[Channel], (AddrForController >> 8) & 0xFF);
+    outb(DmaPageReg[Channel], (DmaAddress >> 16) & 0xFF);
+
+    return ReleaseIoLock(STATUS_SUCCESS, &Irql);
 }
