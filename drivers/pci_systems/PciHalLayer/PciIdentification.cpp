a4506cd49e83deade1aaa3f2a2952bbf6de89b3f
AlienMaster815 <tgrenier.815@gmail.com>
2025-10-16 00:48:53 -0400

    Registry PCI Load Order

--- drivers/pci_systems/PciHalLayer/PciIdentification.cpp
+++ drivers/pci_systems/PciHalLayer/PciIdentification.cpp
@@ -39,7 +39,13 @@ UNUSED static void SanityCheck(LPWSTR Str, size_t Length){
     LouPrint("\n");
 }
 
-static LPWSTR CreateRegistryEntryFromPciDevice(PPCI_COMMON_CONFIG Config){
+static void CreateRegistryEntryFromPciDevice(
+    PPCI_COMMON_CONFIG Config,
+    LPWSTR* WcsVendorID, LPWSTR* WcsDeviceID,
+    LPWSTR* WcsSubVendorID, LPWSTR* WcsSubDeviceID,
+    LPWSTR* WcsClassID, LPWSTR* WcsSubClassID,
+    LPWSTR* WcsProgIf
+){
     LPWSTR Result;
 
     UINT16 DeviceID = Config->Header.DeviceID;
@@ -61,90 +67,303 @@ static LPWSTR CreateRegistryEntryFromPciDevice(PPCI_COMMON_CONFIG Config){
 
     if((Class == 0x01) && (SubClass == 0x06)){
 
-        VendorIDs[0] = L'\\';
-        VendorIDs[1] = L'0';
-        VendorIDs[2] = L'x';
-        _ui64tow_s(VendorID, &VendorIDs[3], 5 * sizeof(WCHAR),16);
+        VendorIDs[0] = L'0';
+        VendorIDs[1] = L'x';
+        _ui64tow_s(VendorID, &VendorIDs[2], 5 * sizeof(WCHAR),16);
 
-        DeviceIDs[0] = L'\\';
-        DeviceIDs[1] = L'0';
-        DeviceIDs[2] = L'x';
-        _ui64tow_s(DeviceID, &DeviceIDs[3], 5 * sizeof(WCHAR),16);
+        DeviceIDs[0] = L'0';
+        DeviceIDs[1] = L'x';
+        _ui64tow_s(DeviceID, &DeviceIDs[2], 5 * sizeof(WCHAR),16);
 
-        SubVendorIDs[0] = L'\\';
-        SubVendorIDs[1] = L'0';
-        SubVendorIDs[2] = L'x';
-        _ui64tow_s(SubVendorID, &SubVendorIDs[3], 5 * sizeof(WCHAR),16);
+        SubVendorIDs[0] = L'0';
+        SubVendorIDs[1] = L'x';
+        _ui64tow_s(SubVendorID, &SubVendorIDs[2], 5 * sizeof(WCHAR),16);
 
-        SubDeviceIDs[0] = L'\\';
-        SubDeviceIDs[1] = L'0';
-        SubDeviceIDs[2] = L'x';
-        _ui64tow_s(SubDeviceID, &SubDeviceIDs[3], 5 * sizeof(WCHAR),16);
+        SubDeviceIDs[0] = L'0';
+        SubDeviceIDs[1] = L'x';
+        _ui64tow_s(SubDeviceID, &SubDeviceIDs[2], 5 * sizeof(WCHAR),16);
 
 
-        Classs[0] = L'\\';
-        Classs[1] = L'0';
-        Classs[2] = L'x';
-        _ui64tow_s(Class, &Classs[3], 5 * sizeof(WCHAR),16);
+        Classs[0] = L'0';
+        Classs[1] = L'x';
+        _ui64tow_s(Class, &Classs[2], 5 * sizeof(WCHAR),16);
 
-        SubClasss[0] = L'\\';
-        SubClasss[1] = L'0';
-        SubClasss[2] = L'x';
-        _ui64tow_s(SubClass, &SubClasss[3], 5 * sizeof(WCHAR),16);
+        SubClasss[0] = L'0';
+        SubClasss[1] = L'x';
+        _ui64tow_s(SubClass, &SubClasss[2], 5 * sizeof(WCHAR),16);
 
-        ProgIfs[0] = L'\\';
-        ProgIfs[1] = L'0';
-        ProgIfs[2] = L'x';
-        _ui64tow_s(ProgIf, &ProgIfs[3], 5 * sizeof(WCHAR),16);
+        ProgIfs[0] = L'0';
+        ProgIfs[1] = L'x';
+        _ui64tow_s(ProgIf, &ProgIfs[2], 5 * sizeof(WCHAR),16);
 
-        Result = LouKeMallocArray(WCHAR, wcslen(L"KERNEL_DEFAULT_CONFIG\\Drivers\\PCI") + wcslen(VendorIDs) + wcslen(DeviceIDs) + wcslen(SubVendorIDs) + wcslen(SubDeviceIDs) + wcslen(Classs) + wcslen(SubClasss) + wcslen(ProgIfs) + 1 , KERNEL_GENERIC_MEMORY);
-
-        wcscpy(Result, L"KERNEL_DEFAULT_CONFIG\\Drivers\\PCI");
-        wcscat(Result, VendorIDs);
-        wcscat(Result, DeviceIDs);
-        wcscat(Result, SubVendorIDs);
-        wcscat(Result, SubDeviceIDs);
-        wcscat(Result, Classs);
-        wcscat(Result, SubClasss);
-        wcscat(Result, ProgIfs);
-
-        return Result;
+        Result = LouKeMallocArray(WCHAR, (wcslen(VendorIDs) + 1) + (wcslen(DeviceIDs) + 1) + (wcslen(SubVendorIDs) + 1) + (wcslen(SubDeviceIDs) + 1) + (wcslen(Classs) + 1) + (wcslen(SubClasss) + 1) + (wcslen(ProgIfs) + 1), KERNEL_GENERIC_MEMORY);
+        *WcsVendorID = Result;         
+        wcscpy(Result, VendorIDs);
+        
+        Result += wcslen(Result) + 1;
+        *WcsDeviceID = Result;
+        wcscpy(Result , DeviceIDs);
+        
+        Result += wcslen(Result) + 1;
+        *WcsSubVendorID = Result;
+        wcscpy(Result, SubVendorIDs);
+        
+        Result += wcslen(Result) + 1;
+        *WcsSubDeviceID = Result;
+        wcscpy(Result, SubDeviceIDs);
+        
+        Result += wcslen(Result) + 1;
+        *WcsClassID = Result;
+        wcscpy(Result, Classs);
+        
+        Result += wcslen(Result) + 1;
+        *WcsSubClassID = Result;
+        wcscpy(Result, SubClasss);
+        
+        Result += wcslen(Result) + 1;
+        *WcsProgIf = Result;
+        wcscpy(Result, ProgIfs);
     }
+}
 
+PVOID LouKeGetPciDeviceRegistryHandle(
+    LPWSTR VendorID, LPWSTR DeviceID,
+    LPWSTR SubVendorID, LPWSTR SubDeviceID,
+    LPWSTR ClassID, LPWSTR SubClassID,
+    LPWSTR ProgIf 
+){
+    PVOID PciKeys = LouKeOpenRegistryHandle(L"KERNEL_DEFAULT_CONFIG\\Drivers\\PCI", 0x00);
+    PVOID VendorKey = 0;
+    PVOID DeviceKey = 0;
+    PVOID SubVendorKey = 0;
+    PVOID SubDeviceKey = 0;
+    PVOID ClassKey = 0;
+    PVOID SubClassKey = 0;
+    PVOID ProgIfKey = 0;
+    if(!PciKeys){
+        LouPrint("Unable To Open PCI Keys\n");
+        return 0x00;
+    }
+    //what im about to do is about to piss
+    //everyone off if you get pissed off by
+    //this either deal with it or make it 
+    //better and exspect me to deny a patch
+    //if it dosent work on my machine...
+    bool VenSpecficKey = true;
+    bool DevSpecficKey = true;
+    bool SubVenSpecficKey = true;
+    bool SubDevSpecficKey = true;
+    bool ClassSpecificKey = true;
+    bool SubClassSpecificKey = true;
+    VendorKey = LouKeOpenRegistryHandle(VendorID, PciKeys);
+    if(!VendorKey){
+        _ABORT_VENDOR_SPECIFC_KEY:
+        VenSpecficKey = false;
+        DevSpecficKey = true;
+        SubVenSpecficKey = true;
+        SubDevSpecficKey = true;
+        ClassSpecificKey = true;
+        SubClassSpecificKey = true;
+        VendorKey = LouKeOpenRegistryHandle(L"0xFFFF", PciKeys);
+        if(!VendorKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    DeviceKey = LouKeOpenRegistryHandle(DeviceID, VendorKey);
+    if(!DeviceKey){
+        _ABORT_DEVICE_SPECIFC_KEY:
+        DevSpecficKey = false;
+        SubVenSpecficKey = true;
+        SubDevSpecficKey = true;
+        ClassSpecificKey = true;
+        SubClassSpecificKey = true;
+        //if device specific Device key is not found use a generic one
+        DeviceKey = LouKeOpenRegistryHandle(L"0xFFFF", VendorKey);
+        if(!DeviceKey && VenSpecficKey){
+            //if we use a vendor specific Vendor Key fallback to a Generic Key
+            goto _ABORT_VENDOR_SPECIFC_KEY;
+        }
+        else if(!DeviceKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    SubVendorKey = LouKeOpenRegistryHandle(SubVendorID, DeviceKey);
+    if(!SubVendorKey){
+        _ABORT_SUB_VENDOR_SPECIFC_KEY:
+        SubVenSpecficKey = false;
+        SubDevSpecficKey = true;
+        ClassSpecificKey = true;
+        SubClassSpecificKey = true;
+        //if a vendor specific key is not found fall back to a generic one
+        SubVendorKey = LouKeOpenRegistryHandle(L"0xFFFF", DeviceKey);
+        if(!SubVendorKey && DevSpecficKey){
+            goto _ABORT_DEVICE_SPECIFC_KEY;
+        }
+        else if(!SubVendorKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    SubDeviceKey = LouKeOpenRegistryHandle(SubDeviceID, SubVendorKey);
+    if(!SubDeviceKey){
+        _ABORT_SUB_DEVICE_SPECIFC_KEY:
+        SubDevSpecficKey = false;
+        ClassSpecificKey = true;
+        SubClassSpecificKey = true;
+        //if device specific Device key is not found use a generic one
+        SubDeviceKey = LouKeOpenRegistryHandle(L"0xFFFF", SubVendorKey);
+        if(!SubDeviceKey && SubVenSpecficKey){
+            //if we use a vendor specific Vendor Key fallback to a Generic Key
+            goto _ABORT_SUB_VENDOR_SPECIFC_KEY;
+        }
+        else if(!SubDeviceKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    ClassKey = LouKeOpenRegistryHandle(ClassID, SubDeviceKey);
+    if(!ClassKey){
+        _ABORT_CLASS_SPECIFC_KEY:
+        ClassSpecificKey = false;
+        SubClassSpecificKey = true;
+        //if device specific Device key is not found use a generic one
+        ClassKey = LouKeOpenRegistryHandle(L"0xFF", SubDeviceKey);
+        if(!ClassKey && SubDevSpecficKey){
+            //if we use a vendor specific Vendor Key fallback to a Generic Key
+            goto _ABORT_SUB_DEVICE_SPECIFC_KEY;
+        }
+        else if(!ClassKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    SubClassKey = LouKeOpenRegistryHandle(SubClassID, ClassKey);
+    if(!SubClassKey){
+        //_ABORT_SUB_CLASS_SPECIFC_KEY:
+        SubClassSpecificKey = false;
+        //if device specific Device key is not found use a generic one
+        SubClassKey = LouKeOpenRegistryHandle(L"0xFF", ClassKey);
+        if(!SubClassKey && ClassSpecificKey){
+            //if we use a vendor specific Vendor Key fallback to a Generic Key
+            goto _ABORT_CLASS_SPECIFC_KEY;
+        }
+        else if(!SubClassKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    ProgIfKey = LouKeOpenRegistryHandle(ProgIf, SubClassKey);
+    if(!ProgIfKey){
+        //if device specific Device key is not found use a generic one
+        ProgIfKey = LouKeOpenRegistryHandle(L"0xFF", SubClassKey);
+        if(!ProgIfKey && SubClassSpecificKey){
+            //if we use a vendor specific Vendor Key fallback to a Generic Key
+            goto _ABORT_CLASS_SPECIFC_KEY;
+        }
+        else if(!ProgIfKey){
+            goto _REGISTRY_SEARCH_IMPLODED;
+        }
+    }
+    return ProgIfKey;
+
+    _REGISTRY_SEARCH_IMPLODED:
     return 0x00;
 }
 
-
-
 size_t LouKePciGetLoadOrderForEntry(
-    LPWSTR Entry
+    LPWSTR VendorID, LPWSTR DeviceID,
+    LPWSTR SubVendorID, LPWSTR SubDeviceID,
+    LPWSTR ClassID, LPWSTR SubClassID,
+    LPWSTR ProgIf
 ){
+    PVOID PciDeviceHandle = 0x00;
+    PVOID LoadOrderHandle = 0x00;
+    PVOID DriverKeyHandle = 0x00;
+    PVOID LoadOrder = 0x00;
+    if(VendorID){
+        
+        PciDeviceHandle = LouKeGetPciDeviceRegistryHandle(
+            VendorID,
+            DeviceID,
+            SubVendorID,
+            SubDeviceID,
+            ClassID,
+            SubClassID,
+            ProgIf
+        );
 
-    PVOID PciKeys = LouKeOpenRegistryHandle(L"KERNEL_DEFAULT_CONFIG\\Drivers\\PCI", 0x00);
-    if(!PciKeys){
+        if(!PciDeviceHandle){
+            goto _NOT_LOADED_DEVICE;
+        }
 
-        LouPrint("Unable To Open PCI Keys\n");
+        DriverKeyHandle = LouKeOpenRegistryHandle(L"DriverKey", PciDeviceHandle);
+        if(!DriverKeyHandle){
+            goto _NOT_LOADED_DEVICE;
+        }
+        
+        size_t WcsDriverKeySize = LouKeGetRegistryKeySize(DriverKeyHandle);
 
-        return GetBootDeviceCount();
+        LPWSTR DriverKey = LouKeMallocArray(WCHAR, WcsDriverKeySize, KERNEL_GENERIC_MEMORY);
+        UNUSED LOUSTATUS ReadResult = LouKeReadRegistryWcsValue(DriverKeyHandle, DriverKey);
+        if(ReadResult != STATUS_SUCCESS){
+            goto _NOT_LOADED_DEVICE;
+        }
+
+        LoadOrderHandle = LouKeOpenRegistryHandle(DriverKey, 0x00);
+        if(!LoadOrderHandle){
+            goto _NOT_LOADED_DEVICE;
+        }
+
+        LoadOrder = LouKeOpenRegistryHandle(L"LoadOrder", LoadOrderHandle);
+        if(!LoadOrder){
+            LoadOrder = LouKeOpenRegistryHandle(L"PreLoadedFallback", LoadOrderHandle); //generic fallback
+            if(!LoadOrder){
+                goto _NOT_LOADED_DEVICE;
+            }
+        }
+        WORD LoadID = 0x00;
+        ReadResult = LouKeReadRegistryWordValue(LoadOrder, &LoadID);
+        if(ReadResult != STATUS_SUCCESS){
+            goto _NOT_LOADED_DEVICE;
+        }
+        LouKeFree(VendorID);
+        return (size_t)LoadID;
     }
-
-    LouPrint("YAY!!!\n");
-
-    while(1);
+    _NOT_LOADED_DEVICE:
+    if(VendorID){
+        LouKeFree(VendorID);
+    }
     return GetBootDeviceCount();
 }
 
 size_t LouKeGetBootDeviceIndex(PPCI_COMMON_CONFIG Config){
+    size_t Result = 0;
+    LPWSTR VendorID = 0; 
+    LPWSTR DeviceID = 0;
+    LPWSTR SubVendorID = 0; 
+    LPWSTR SubDeviceID = 0;
+    LPWSTR ClassID = 0; 
+    LPWSTR SubClassID = 0;
+    LPWSTR ProgIf = 0;
 
-    LPWSTR Tmp = CreateRegistryEntryFromPciDevice(Config);
+    CreateRegistryEntryFromPciDevice(
+        Config,
+        &VendorID,
+        &DeviceID,
+        &SubVendorID,
+        &SubDeviceID,
+        &ClassID,
+        &SubClassID,
+        &ProgIf
+    );
 
-    if(Tmp){
-        size_t Result = LouKePciGetLoadOrderForEntry(Tmp);
+    Result = LouKePciGetLoadOrderForEntry(
+        VendorID,
+        DeviceID,
+        SubVendorID,
+        SubDeviceID,
+        ClassID,
+        SubClassID,
+        ProgIf
+    );
 
-        LouPrint("LoadOrder:%d\n", Result);
+    LouPrint("Result:%d\n", Result);
 
-        while(1);
-    }
-
-    return GetBootDeviceCount();
+    return Result;
 }
