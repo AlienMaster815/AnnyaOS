20ad4b46f4dad45ca6890d2434ee4d137434d4c6
AlienMaster815 <tgrenier.815@gmail.com>
2026-03-15 15:06:23 -0400

    code cleanup

--- include/Ldm/ddk.h
+++ include/Ldm/ddk.h
@@ -600,6 +600,39 @@ typedef enum _IO_PAGING_PRIORITY{
 struct _FILE_OBJECT;
 struct _IRP;
 
+typedef struct _SYSENV_VALUE{
+	ULONG 	Attributes;
+	ULONG 	ValueLength;
+	UCHAR* 	ValueData;
+}SYSENV_VALUE, * PSYSENV_VALUE;
+
+typedef struct _SYSENV_VARIABLE{
+	GUID  	VendorGuid;
+	ULONG 	VariableNameLength;
+	WCHAR* 	VariableName;
+}SYSENV_VARIABLE, * PSYSENV_VARIABLE;
+
+typedef struct _SYSENV_VARIABLE_INFO{
+	ULONGLONG 	MaximumVariableStorageSize;
+	ULONGLONG 	RemainingVariableStorageSize;
+	ULONGLONG 	MaximumVariableSize;
+} SYSENV_VARIABLE_INFO, * PSYSENV_VARIABLE_INFO;
+
+typedef struct _XVARIABLE_NAME{
+	ULONG 	NextEntryOffset;
+	GUID  	VendorGuid;
+	WCHAR* 	Name;
+}XVARIABLE_NAME, * PXVARIABLE_NAME;
+
+typedef struct _XVARIABLE_NAME_AND_VALUE{
+	ULONG 	NextEntryOffset;
+	ULONG 	ValueOffset;
+	ULONG 	ValueLength;
+	ULONG 	Attributes;
+	GUID  	VendorGuid;
+	WCHAR* 	Name;
+}XVARIABLE_NAME_AND_VALUE, * PXVARIABLE_NAME_AND_VALUE;
+
 #ifndef _USER_MODE_CODE_
 
 KERNEL_EXPORT LOUSTATUS LouKeIoAllocateAdapterChannel(PADAPTER_OBJECT AdapterObject, PDEVICE_OBJECT DeviceObject, ULONG NumberOfMapRegisters, PDRIVER_CONTROL ExecutionRoutine, PVOID Context);
--- include/WinAPI/NtAPI/NtAPIFunc.h
+++ include/WinAPI/NtAPI/NtAPIFunc.h
@@ -3,12 +3,10 @@
 #include <NtAPI.h>
 #pragma once
 
-#include "ntddsysenv.h"
 #include "ntifs.h"
 #include "wmistr.h"
 #include "wmilib.h"
 #include "irp.h"
-#include "ntddsysenv.h"
 #include "Ntintsafe.h"
 #include "ntoapi.h"
 #include "ntifs.h"
