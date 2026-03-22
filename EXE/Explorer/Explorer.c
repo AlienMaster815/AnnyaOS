e058b7a02cc6a9631d24933b5f8389f5688e10f9
AlienMaster815 <tgrenier.815@gmail.com>
2026-03-22 01:19:48 -0400

    Update Explorer.c

--- EXE/Explorer/Explorer.c
+++ EXE/Explorer/Explorer.c
@@ -1,12 +1,84 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
 #include <windows.h>
+
+const WCHAR DESKTOP_WINDOW_CLASS_NAME[] =  L"AnnyaExp Desktop Window Class";
+
+LRESULT CALLBACK DesktopWindowProcedure(HWND Window, UINT Message, WPARAM WordParam, LPARAM LongParam);
+
+static HBRUSH DefaultDesktopBackGround; 
 
 int WINAPI WinMain(
     HINSTANCE   Instance,
-    HINSTANCE   Previous,
+    HINSTANCE   PrevInstance,
     LPSTR       CommandLine,
-    int         CmdShow
+    int         nCmdShow
 ) {
     
-    while(1);
-    return 0x00;
+    DefaultDesktopBackGround = CreateSolidBrush(RGB(0, 128, 128)); 
+    WNDCLASS Wc = {0};
+    Wc.lpfnWndProc = DesktopWindowProcedure;
+    Wc.hInstance = Instance;
+    Wc.lpszClassName = DESKTOP_WINDOW_CLASS_NAME;
+    Wc.hbrBackground = DefaultDesktopBackGround;
+    
+    RegisterClass(&Wc);
+
+    HWND DesktopWindow = CreateWindowEx(
+        0,
+        DESKTOP_WINDOW_CLASS_NAME,
+        L"AnnyaExp Desktop Window",
+        WS_POPUP | WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        600,
+        480,
+        NULL,
+        NULL,
+        Instance,
+        NULL
+    );
+
+    if(!DesktopWindow){
+        return 0x00;
+    }
+
+    ShowWindow(DesktopWindow, nCmdShow);
+    UpdateWindow(DesktopWindow);
+
+    MSG Message = {0};
+
+    while(GetMessage(&Message, NULL, 0 , 0 ) > 0){
+        TranslateMessage(&Message);
+        DispatchMessage(&Message);
+    }
+
+    return 0;
+}
+
+LRESULT 
+CALLBACK 
+DesktopWindowProcedure(
+    HWND    Window, 
+    UINT    Message, 
+    WPARAM  WordParam, 
+    LPARAM  LongParam
+){
+
+    switch(Message){
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            return 0;
+        //case WM_PAINT:{
+        //    PAINTSTRUCT Ps;
+        //    HDC Hdc = BeginPaint(Window, &Ps);
+        //    FillRect(Hdc, &Ps.rcPaint, (HBRUSH)(COLOR_WINDOW+1));
+        //    EndPaint(Window, &Ps);
+        //    return 0;
+        //}
+    }
+
+    return DefWindowProc(Window,Message, WordParam, LongParam);
 }
