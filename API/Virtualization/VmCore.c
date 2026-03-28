a4b7c05f529d00f39d2c3e84c080e1d96228d971
AlienMaster815 <tgrenier.815@gmail.com>
2026-03-27 21:07:52 -0400

    VmCore Update

---
+++ API/Virtualization/VmCore.c
@@ -0,0 +1,11 @@
+#include "VmCore.h"
+
+
+KERNEL_ENTRY 
+LOUSTATUS 
+VmCoreEntry(){
+
+
+
+    return STATUS_SUCCESS;
+}
---
+++ API/Virtualization/VmCore.h
@@ -0,0 +1,9 @@
+#ifndef _VM_CORE_INTERNAL_API_H
+#define _VM_CORE_INTERNAL_API_H
+#define _KERNEL_MODULE_
+
+#include <LouDDK.h>
+
+
+
+#endif
