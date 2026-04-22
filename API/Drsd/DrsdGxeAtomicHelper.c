30fa0358aedf91b5a171e8a688790ab3d78f7dfe
AlienMaster815 <tgrenier.815@gmail.com>
2026-04-22 03:40:39 -0400

    Update DrsdGxeAtomicHelper.c

--- API/Drsd/DrsdGxeAtomicHelper.c
+++ API/Drsd/DrsdGxeAtomicHelper.c
@@ -22,6 +22,10 @@ DrsdGxeEndShadowFbAccess(
     PDRSD_PLANE         Plane,
     PDRSD_PLANE_STATE   State
 ){
-    LouPrint("DRSDCORE.SYS:DrsdGxeEndShadowFbAccess()\n");
-    while(1);
+    PDRSD_SHADOW_PLANE_STATE ShadowPlaneState = ToDrsdShadowPlaneState(PlaneState);
+    PDRSD_FRAME_BUFFER Fb = PlaneState->FrameBuffer;
+    if(!Fb){
+        return;
+    }
+    DrsdGxeFbVUnMap(Fb, ShadowPlaneState->Map, ShadowPlaneState->Data);
 }
